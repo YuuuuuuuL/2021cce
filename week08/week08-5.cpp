8392ba3e8d914c3cde421e5a64a5bbd010c236c1
Yu <91170834+YuuuuuuuL@users.noreply.github.com>
2021-11-03 11:55:34 +0800

    Add files via upload

---
+++ week08/week08-5.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int n = 6;
+    for(int i = 1; i <= n; i++){
+        printf("%d: ", i);
+        for(int k = 1; k <= i; k++)
+            printf("*");
+            printf("\n");
+
+    }
+}
+
